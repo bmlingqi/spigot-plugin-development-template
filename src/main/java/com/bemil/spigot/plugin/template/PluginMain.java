dd88fc854e4a49f9ab51e29370390795f52f341b
bmlingqi <bemiou_lingqi@163.com>
2022-11-05 16:30:32 +0800

    Minecraft 1.18.8 Spigot plugin project initialization

---
+++ src/main/java/com/bemil/spigot/plugin/template/PluginMain.java
@@ -0,0 +1,29 @@
+package com.bemil.spigot.plugin.template;
+
+import com.sun.org.slf4j.internal.LoggerFactory;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+/**
+ * @FileName: PluginMain
+ * @Description: 插件主类
+ * @Author: Bemil_Lingqi <bemiou_lingqi@163.com>
+ * @Date 2022/11/5
+ */
+public class PluginMain extends JavaPlugin {
+    @Override
+    public void onLoad() {
+        //TODO The plug-in loading code is written here
+    }
+
+    @Override
+    public void onEnable() {
+        //TODO The plug-in enabler code is written here
+    }
+
+    @Override
+    public void onDisable() {
+        //TODO The plug-in uninstall code is written here
+    }
+}
